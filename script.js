0342444d942e8223982334ba5e232e537a5f3726
Hayley <hb_060492@hotmail.co.uk>
2023-01-09 20:31:57 +0000

    dealer sticks at 21 instead of win, optimised random num funct

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
+//TODO create game object that stores player and dealer info
+//TODO change deal random number generators so the numbers are parameters. eg numberGen(min, max) {Math.floor((Math.random() * min) + max)}
+//TODO HOWEVER would be better to have an array deckOfCards and use splice to take a random element out. 2 elements for player first hand, then 1 element after. Ensures no chance of being dealt like 8 2s in a row, more like a real blackjack game.
+//TODO functions for phrases like you win, you lose, confirm playAgain etc.
+
+
 //Need variables for keeping score of both player and dealer
 var playerScore = 0;
 var dealerScore = 0;
 
 //FUNCTIONS
-//function to deal random number between 2-11
-function deal211() {
-    var hand = Math.floor((Math.random() * 10) + 2);
-    return hand;
-} 
-//function to deal random number between 4-21
-function deal421() {
-    var hand = Math.floor((Math.random() * 18) + 4);
+//function to deal random number
+function dealRandom(num1, num2) {
+    var hand = Math.floor((Math.random() * num2) + num1);
     return hand;
 }
-//!Tried to create function to compare hand to 21, but it didn't save any lines of code.
-//!Could not get scores to change within the function, and would need too many parameters to be reused for both player and dealer.
 
 //wraps whole game in function so it can be replayed
 function playGame() {
 
 //Deal random number between 4-21 to player, and number between 2-11 to dealer.
-var playerHand = deal421();
-var dealerHand = deal211();
+var playerHand = dealRandom(4,18);
+var dealerHand = dealRandom(2,10);
 
 //if statement so dealer's hand only shown if player does not get 21 in first round.
 if (playerHand === 21) {
@@ -32,7 +31,7 @@ else if (playerHand < 21) {
     alert("You have been dealt " + playerHand + ".");
     var playerChoice = confirm("You have " + playerHand + " and the dealer has " + dealerHand + ". Select ok to be dealt another card, or cancel to stick.");
     while (playerChoice) {
-        var newCard = deal211();
+        var newCard = dealRandom(2,10);
         var playerHand = playerHand + newCard;
         alert("You have been dealt " + newCard + ".");
         if (playerHand === 21) {
@@ -50,22 +49,18 @@ else if (playerHand < 21) {
         }
     }
     //player has stuck, now dealer gets their cards
+    //DONE removed the scenario where dealer reaches 21 and wins automatically - will now just stick.
     if (!playerChoice) {
         while (dealerHand < 17) {
-            var dealerCard = deal211();
+            var dealerCard = dealRandom(2,10);
             var dealerHand = dealerHand + dealerCard;
             alert("Dealer was dealt " + dealerCard + ". The dealer now has " + dealerHand + ".");
-            if (dealerHand === 21) {
-                dealerScore++
-                alert("Dealer has " + dealerHand + ". You have lost this round.");
-                break;
-            }
-            else if (dealerHand > 21) {
+            if (dealerHand > 21) {
                 playerScore++
                 alert("Dealer has " + dealerHand + ". They have gone bust. You win this round!");
                 break;
             }
-            else if (dealerHand >= 17 && dealerHand < 21) {
+            else if (dealerHand >= 17 && dealerHand <= 21) {
                 alert("Dealer has " + dealerHand + " and is sticking.");
                 //Dealer has stuck, now compare scores to find winner
                 alert("Dealer has " + dealerHand + " and you have " + playerHand + ".");
