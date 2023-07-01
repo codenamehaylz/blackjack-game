90814e5ebe32837cbfce508848707656d7292060
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-07-01 18:46:27 +0100

    dealRandom function now uses min max as arguments, updated variables to const/let

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-//TODO create game object that stores player and dealer info
-//TODO change deal random number generators so the numbers are parameters. eg numberGen(min, max) {Math.floor((Math.random() * min) + max)}
-//TODO HOWEVER would be better to have an array deckOfCards and use splice to take a random element out. 2 elements for player first hand, then 1 element after. Ensures no chance of being dealt like 8 2s in a row, more like a real blackjack game.
-//TODO functions for phrases like you win, you lose, confirm playAgain etc.
-
-
-//Need variables for keeping score of both player and dealer
-var playerScore = 0;
-var dealerScore = 0;
+//Variables for keeping score of both player and dealer
+let playerScore = 0;
+let dealerScore = 0;
 
 //FUNCTIONS
 //function to deal random number
-function dealRandom(num1, num2) {
-    var hand = Math.floor((Math.random() * num2) + num1);
+function dealRandom(min, max) {
+    const hand = Math.floor((Math.random() * (max - min)) + min);
     return hand;
 }
 
@@ -19,8 +13,8 @@ function dealRandom(num1, num2) {
 function playGame() {
 
 //Deal random number between 4-21 to player, and number between 2-11 to dealer.
-var playerHand = dealRandom(4,18);
-var dealerHand = dealRandom(2,10);
+let playerHand = dealRandom(4,18);
+let dealerHand = dealRandom(2,10);
 
 //if statement so dealer's hand only shown if player does not get 21 in first round.
 if (playerHand === 21) {
@@ -29,10 +23,10 @@ if (playerHand === 21) {
 }
 else if (playerHand < 21) {
     alert("You have been dealt " + playerHand + ".");
-    var playerChoice = confirm("You have " + playerHand + " and the dealer has " + dealerHand + ". Select ok to be dealt another card, or cancel to stick.");
+    let playerChoice = confirm("You have " + playerHand + " and the dealer has " + dealerHand + ". Select ok to be dealt another card, or cancel to stick.");
     while (playerChoice) {
-        var newCard = dealRandom(2,10);
-        var playerHand = playerHand + newCard;
+        const newCard = dealRandom(2,10);
+        playerHand = playerHand + newCard;
         alert("You have been dealt " + newCard + ".");
         if (playerHand === 21) {
             alert("You now have " + playerHand + " and will stick.");
@@ -40,7 +34,7 @@ else if (playerHand < 21) {
             break;
         }
         else if (playerHand < 21) {
-            var playerChoice = confirm("You now have " + playerHand + ". Select ok to be dealt another card, or cancel to stick.");
+            playerChoice = confirm("You now have " + playerHand + ". Select ok to be dealt another card, or cancel to stick.");
         }
         else if (playerHand > 21) {
             dealerScore++
@@ -52,8 +46,8 @@ else if (playerHand < 21) {
     //DONE removed the scenario where dealer reaches 21 and wins automatically - will now just stick.
     if (!playerChoice) {
         while (dealerHand < 17) {
-            var dealerCard = dealRandom(2,10);
-            var dealerHand = dealerHand + dealerCard;
+            const dealerCard = dealRandom(2,10);
+            dealerHand = dealerHand + dealerCard;
             alert("Dealer was dealt " + dealerCard + ". The dealer now has " + dealerHand + ".");
             if (dealerHand > 21) {
                 playerScore++
@@ -86,16 +80,18 @@ else if (playerHand < 21) {
 //Triggers game to begin
 alert("Let's play Blackjack!")
 playGame();
-var playAgain = confirm("Would you like to play another round?");
+const playAgain = confirm("Would you like to play another round?");
 while (playAgain) {
     playGame();
-    var playAgain = confirm("Would you like to play another round?");
+    playAgain = confirm("Would you like to play another round?");
 }
 if (!playAgain) {
     alert("Thanks for playing! You won " + playerScore + " time(s), and the dealer won " + dealerScore + " time(s).");
 }
 
 console.log("Dealer's score: " + dealerScore);
-console.log("Player's score : " + playerScore);
+console.log("Player's score: " + playerScore);
 
-//TODO code to make game playable on the webpage instead of prompts
+
+//TODO ake game playable on the page instead of prompts
+//TODO create game object that stores player and dealer info
